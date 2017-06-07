5202aac3776cbd91ff4413638e0aeb98617d0ee0
Trim Bresilla <trim.bresilla@gmail.com>
2017-06-07 17:11:28 +0200

    initialise :)

---
+++ build/publisher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/trim/CODE/ROBO/part_2/robots_with_ROS/src/publisher
+# Build directory: /home/trim/CODE/ROBO/part_2/robots_with_ROS/build/publisher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
